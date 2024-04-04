47e1110b2ef99c9edcd6ef0bc9a563f070293b2a
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2024-04-04 11:49:09 +0200

    blob reader

--- src/DecryptEncrypt/InMemoryTarLoader.java
+++ src/DecryptEncrypt/InMemoryTarLoader.java
@@ -1,47 +1,49 @@
 package DecryptEncrypt;
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
-import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-public class InMemoryTarLoader {
-    import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
-import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+public class InMemoryTarLoader2 {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("path_to_your_tar_file")) {
+            InMemoryTarLoader2 loader = new InMemoryTarLoader2();
+            loader.loadTar(fis);
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+            Map<String, byte[]> files = loader.getFiles();
+            for (String path : files.keySet()) {
+                System.out.println("File path: " + path);
+                byte[] content = files.get(path);
+                // Verwenden Sie den Inhalt der Datei...
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-    public class InMemoryLoader {
-        private Map<String, byte[]> files = new HashMap<>();
+    private Map<String, byte[]> files = new HashMap<>();
 
-        public void loadTar(InputStream tarInputStream) throws IOException {
-            try (TarArchiveInputStream tarInput = new TarArchiveInputStream(tarInputStream)) {
-                TarArchiveEntry currentEntry;
-                while ((currentEntry = tarInput.getNextTarEntry()) != null) {
-                    if (!currentEntry.isDirectory()) {
-                        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                        byte[] buffer = new byte[1024];
-                        int read;
-                        while ((read = tarInput.read(buffer, 0, buffer.length)) != -1) {
-                            bos.write(buffer, 0, read);
-                        }
-                        files.put(currentEntry.getName(), bos.toByteArray());
+    public void loadTar(InputStream tarInputStream) throws IOException {
+        try (TarArchiveInputStream tarInput = new TarArchiveInputStream(tarInputStream)) {
+            TarArchiveEntry currentEntry;
+            while ((currentEntry = tarInput.getNextTarEntry()) != null) {
+                if (!currentEntry.isDirectory()) {
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    byte[] buffer = new byte[1024];
+                    int read;
+                    while ((read = tarInput.read(buffer, 0, buffer.length)) != -1) {
+                        bos.write(buffer, 0, read);
                     }
+                    files.put(currentEntry.getName(), bos.toByteArray());
                 }
             }
         }
+    }
 
-        public Map<String, byte[]> getFiles() {
-            return files;
-        }
+    public Map<String, byte[]> getFiles() {
+        return files;
     }
 }
-
-
