086d3dbc976f69629af8ff75e7a976caaf5f4e59
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2024-04-05 08:37:10 +0200

    xml durchlaufen

--- src/DecryptEncrypt/DeCompressByteArray.java
+++ src/DecryptEncrypt/DeCompressByteArray.java
@@ -2,8 +2,14 @@ package DecryptEncrypt;
 
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.crypto.Cipher;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -61,6 +67,32 @@ public class DeCompressByteArray {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public void listXmlContent(byte[] xmlData) {
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(new ByteArrayInputStream(xmlData));
+
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+            NodeList nList = doc.getElementsByTagName("*");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("Node Name :" + eElement.getNodeName() + " [OPEN]");
+                    System.out.println("Node Value :" + eElement.getTextContent());
+                    System.out.println("Node Name :" + eElement.getNodeName() + " [CLOSE]");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
