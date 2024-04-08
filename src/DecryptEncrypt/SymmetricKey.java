d49b3914c25e2d8391777a6fe6caed3ced6e1399
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2024-04-08 08:26:55 +0200

    schluessel erzeugen

--- src/DecryptEncrypt/SymmetricKey.java
+++ src/DecryptEncrypt/SymmetricKey.java
@@ -1,11 +1,12 @@
 package DecryptEncrypt;
 
+import javax.crypto.*;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
 
 public class SymmetricKey {
 
@@ -14,6 +15,32 @@ public class SymmetricKey {
         keyGen.init(128); // for example
         SecretKey secretKey = keyGen.generateKey();
         return secretKey;
+    }
+
+    public byte[] encryptKeyWithPublicKey(SecretKey secretKey, PublicKey publicKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        // Get the bytes of the secret key
+        byte[] keyBytes = secretKey.getEncoded();
+
+        // Create a Cipher instance for RSA
+        Cipher cipher = Cipher.getInstance("RSA");
+
+        // Initialize the Cipher with the public key for encryption
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        // Encrypt the key
+        byte[] encryptedKeyBytes = cipher.doFinal(keyBytes);
+
+        return encryptedKeyBytes;
+    }
+
+    public byte[] generateSHA256Hash(byte[] encryptedKeyBytes) throws NoSuchAlgorithmException {
+        // Create a MessageDigest instance for SHA-256
+        MessageDigest sha256Digest = MessageDigest.getInstance("SHA-256");
+
+        // Calculate the SHA-256 hash of the encrypted key bytes
+        byte[] hash = sha256Digest.digest(encryptedKeyBytes);
+
+        return hash;
     }
 
     public byte[] generateSHA256Hash(SecretKey secretKey) throws NoSuchAlgorithmException {
